6e1ea1c2b9887770dfe5444fe419a8d4623f6f6e
mabirizi <mabiriziad@gmail.com>
2019-09-13 19:44:53 +0300

    Creating an App bar and the body

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('HelloWorld!'),);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my first App'),
+      ),
+    );
   }
 }
