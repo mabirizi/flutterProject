33e7c354d06831f2b1e9b7d079183916617746e6
mabirizi <mabiriziad@gmail.com>
2019-09-13 20:39:11 +0300

    Anonymous functions on the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -29,11 +29,14 @@ class MyApp extends StatelessWidget {
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: () => print('Answer 2 chosen!'),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: () {
+                //.....
+                print('Answer 3 Chosen');
+              },
             ),
           ],
         ),
