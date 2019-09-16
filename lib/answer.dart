ecd6bcaef76d4fa46684c794c02dc56b682345bd
mabirizi <mabiriziad@gmail.com>
2019-09-16 13:51:49 +0300

    Introducing maps and mapping lists to widgets

--- lib/answer.dart
+++ lib/answer.dart
@@ -5,13 +5,14 @@ class Answer extends StatelessWidget {
   final Function selectHandler;
 
   Answer(this.selectHandler);
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
         color: Colors.blue,
+        textColor: Colors.white,
         child: Text('Answer 1'),
         onPressed: selectHandler,
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,16 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s  your favourite animal?'
+      {'questionText': 'What\'s your favourite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {'questionText': 'What\'s your favourite animal?',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      },
+      {'questionText': 'What\'s your favourite Instructor?',
+        'answers': ['Max', 'Manuel', 'Sarah', 'Stephen'],
+      },
+      
     ];
     return MaterialApp(
       home: Scaffold(
