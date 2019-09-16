c7a86dda05ae5ea068d89b81d13fe0df777f7535
mabirizi <mabiriziad@gmail.com>
2019-09-16 11:45:12 +0300

    creating a new an custom stateless widget question

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
+//package imports
 import 'package:flutter/material.dart';
+
+//file imports
+import './question.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
-@override
+  @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
-
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
-
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    
 
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -38,10 +38,10 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Question(questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
