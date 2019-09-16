4d5bcb173b602ed946a96935883305c6589036ea
mabirizi <mabiriziad@gmail.com>
2019-09-16 13:26:38 +0300

    creating a new an custom stateless widget Answer and passing the question

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 //file imports
 import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,21 +40,10 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                //.....
-                print('Answer 3 Chosen');
-              },
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            
           ],
         ),
       ),
