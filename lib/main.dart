4190c09ec4559d523fb60efadc1673472d586f33
mabirizi <mabiriziad@gmail.com>
2019-09-16 11:15:55 +0300

    Updating the widget data and changing form stateless to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
 
   void answerQuestion() {
-    print('Answer chosen!');
+
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    
+
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-
     var questions = [
       'What\'s your favorite color?',
       'What\'s  your favourite animal?'
@@ -22,7 +38,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Question!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
